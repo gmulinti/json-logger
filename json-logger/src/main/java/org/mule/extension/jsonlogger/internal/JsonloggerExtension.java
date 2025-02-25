e17d54289c61427983dfa2359510a2b5f52789c8
Govind Mulinti <govindsm@yahoo.com>
2025-02-25 10:37:21 +0000

    lean package
    removed dependencies and extns for apmq,jms and amqp

--- json-logger/src/main/java/org/mule/extension/jsonlogger/internal/JsonloggerExtension.java
+++ json-logger/src/main/java/org/mule/extension/jsonlogger/internal/JsonloggerExtension.java
@@ -1,9 +1,6 @@
 package org.mule.extension.jsonlogger.internal;
 
-import org.mule.extension.jsonlogger.internal.destinations.AMQDestination;
-import org.mule.extension.jsonlogger.internal.destinations.AMQPDestination;
 import org.mule.extension.jsonlogger.internal.destinations.Destination;
-import org.mule.extension.jsonlogger.internal.destinations.JMSDestination;
 import org.mule.runtime.extension.api.annotation.Export;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.Configurations;
@@ -20,8 +17,6 @@ import org.mule.sdk.api.meta.JavaVersion;
 @Extension(name = "JSON Logger")
 @Export(resources = {"modules/JSONLoggerModule.dwl"})
 @Configurations(JsonloggerConfiguration.class)
-@SubTypeMapping(baseType = Destination.class,
-        subTypes = {JMSDestination.class, AMQDestination.class, AMQPDestination.class})
 @JavaVersionSupport({JavaVersion.JAVA_8, JavaVersion.JAVA_11, JavaVersion.JAVA_17})
 public class JsonloggerExtension {
 
